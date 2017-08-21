5e0b8d2a2c2cdba847165c281a404d485072d90f
YaChun07 <yachun07@gmail.com>
2017-08-21 09:42:33 +0800

    One thing one test

--- ClassLibrary2/ClassLibrary2Tests/Class1Tests.cs
+++ ClassLibrary2/ClassLibrary2Tests/Class1Tests.cs
@@ -11,32 +11,49 @@ namespace StringAverage.Tests
     public class StringAvg
     {
         [TestMethod()]
-        public void Four_AverageStringTest()
+        public void Four_HasZero_FourNum_AverageStringTest()
         {
             Assert.AreEqual("four", StringAverage.AverageString("zero nine five two"));
+        }
+        [TestMethod()]
+        public void Four_NoZero__TwoNum_AverageStringTest()
+        {
             Assert.AreEqual("four", StringAverage.AverageString("five four"));
         }
         [TestMethod()]
-        public void Three_AverageStringTest()
+        public void Three_HasZero_FourNum_AverageStringTest()
         {
 
-            Assert.AreEqual("three", StringAverage.AverageString("four six two three"));
+            Assert.AreEqual("three", StringAverage.AverageString("four eight zero three"));
+        }
+        [TestMethod()]
+        public void Three_NoZero_FiveNum_AverageStringTest()
+        {
             Assert.AreEqual("three", StringAverage.AverageString("one two three four five"));
         }
         [TestMethod()]
-        public void zero_AverageStringTest()
+        public void zero_AllZero_AverageStringTest()
         {
 
             Assert.AreEqual("zero", StringAverage.AverageString("zero zero zero zero zero"));
 
         }
         [TestMethod()]
-        public void na_AverageStringTest()
+        public void na_Null_AverageStringTest()
         {
 
             Assert.AreEqual("n/a", StringAverage.AverageString(""));
-            Assert.AreEqual("n/a", StringAverage.AverageString("eight five six zero qjeoxp one one four qjeoxp"));
+        }
+        [TestMethod()]
+        public void na_WrongInput_AverageStringTest()
+        {
+
             Assert.AreEqual("n/a", StringAverage.AverageString("qjeoxp"));
+        }
+        [TestMethod()]
+        public void na_WrongInput_WithRightInput_AverageStringTest()
+        {
+            Assert.AreEqual("n/a", StringAverage.AverageString("eight five six zero qjeoxp one one four qjeoxp"));
         }
     }
 }
